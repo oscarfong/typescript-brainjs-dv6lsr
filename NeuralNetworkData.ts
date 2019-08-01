f6b7e23443eb18e8fd705c40995cc98d91c6f671
oscarfong <oscar.fong@gmail.com>
2019-08-01 14:51:19 +0800

    NeuralNetworkData.ts added

---
+++ NeuralNetworkData.ts
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+//import {ResourceBase} from './ClsTest1'
+
+
+class NeuralNetworkItem {
+  labelName: string; 
+  val01: number; // from 0 to 1
+  
+
+  constructor(){
+    //super();
+  }
+
+}
