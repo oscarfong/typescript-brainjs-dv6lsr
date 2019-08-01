6f8d710806fdd6a83cb1e5224c539a0f91feeeaf
oscarfong <oscar.fong@gmail.com>
2019-08-01 15:52:21 +0800

    lib added. console log found

--- NeuralNetworkData.ts
+++ NeuralNetworkData.ts
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 //import {ResourceBase} from './ClsTest1'
 
+// pls ref: to react-ts-lodash-private-95fpq7-8cyqgs
 
 class NeuralNetworkItem {
   labelName: string; 
@@ -10,6 +11,14 @@ class NeuralNetworkItem {
 
   constructor(){
     //super();
+    
+  }
+
+  toJSON() {
+    return JSON.stringify(( [this.labelName, this.val01]))
   }
 
 }
+console.log( "NeuralNetworkItem...");
+
+export default NeuralNetworkItem;
--- index.ts
+++ index.ts
@@ -4,6 +4,10 @@
 // Import stylesheets
 import './style.css';
 
+import NeuralNetworkItem from './NeuralNetworkData';
+
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+var nn1 = new NeuralNetworkItem();
